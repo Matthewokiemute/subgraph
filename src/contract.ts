ccaac531839cdece9994392f506ceab976ae10ef
Matthewokiemute <ego.okiemute@gmail.com>
2023-09-15 14:28:17 +0100

    Initial commit

---
+++ src/contract.ts
@@ -0,0 +1,123 @@
+import {
+  Approval as ApprovalEvent,
+  Claim as ClaimEvent,
+  DelegateChanged as DelegateChangedEvent,
+  DelegateVotesChanged as DelegateVotesChangedEvent,
+  MerkleRootChanged as MerkleRootChangedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  Transfer as TransferEvent
+} from "../generated/Contract/Contract"
+import {
+  Approval,
+  Claim,
+  DelegateChanged,
+  DelegateVotesChanged,
+  MerkleRootChanged,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/schema"
+
+export function handleApproval(event: ApprovalEvent): void {
+  let entity = new Approval(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.owner = event.params.owner
+  entity.spender = event.params.spender
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleClaim(event: ClaimEvent): void {
+  let entity = new Claim(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.claimant = event.params.claimant
+  entity.amount = event.params.amount
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateChanged(event: DelegateChangedEvent): void {
+  let entity = new DelegateChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegator = event.params.delegator
+  entity.fromDelegate = event.params.fromDelegate
+  entity.toDelegate = event.params.toDelegate
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleDelegateVotesChanged(
+  event: DelegateVotesChangedEvent
+): void {
+  let entity = new DelegateVotesChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.delegate = event.params.delegate
+  entity.previousBalance = event.params.previousBalance
+  entity.newBalance = event.params.newBalance
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleMerkleRootChanged(event: MerkleRootChangedEvent): void {
+  let entity = new MerkleRootChanged(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.merkleRoot = event.params.merkleRoot
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTransfer(event: TransferEvent): void {
+  let entity = new Transfer(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.from = event.params.from
+  entity.to = event.params.to
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/contract-utils.ts
@@ -0,0 +1,168 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt, Bytes } from "@graphprotocol/graph-ts"
+import {
+  Approval,
+  Claim,
+  DelegateChanged,
+  DelegateVotesChanged,
+  MerkleRootChanged,
+  OwnershipTransferred,
+  Transfer
+} from "../generated/Contract/Contract"
+
+export function createApprovalEvent(
+  owner: Address,
+  spender: Address,
+  value: BigInt
+): Approval {
+  let approvalEvent = changetype<Approval>(newMockEvent())
+
+  approvalEvent.parameters = new Array()
+
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("spender", ethereum.Value.fromAddress(spender))
+  )
+  approvalEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return approvalEvent
+}
+
+export function createClaimEvent(claimant: Address, amount: BigInt): Claim {
+  let claimEvent = changetype<Claim>(newMockEvent())
+
+  claimEvent.parameters = new Array()
+
+  claimEvent.parameters.push(
+    new ethereum.EventParam("claimant", ethereum.Value.fromAddress(claimant))
+  )
+  claimEvent.parameters.push(
+    new ethereum.EventParam("amount", ethereum.Value.fromUnsignedBigInt(amount))
+  )
+
+  return claimEvent
+}
+
+export function createDelegateChangedEvent(
+  delegator: Address,
+  fromDelegate: Address,
+  toDelegate: Address
+): DelegateChanged {
+  let delegateChangedEvent = changetype<DelegateChanged>(newMockEvent())
+
+  delegateChangedEvent.parameters = new Array()
+
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam("delegator", ethereum.Value.fromAddress(delegator))
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "fromDelegate",
+      ethereum.Value.fromAddress(fromDelegate)
+    )
+  )
+  delegateChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "toDelegate",
+      ethereum.Value.fromAddress(toDelegate)
+    )
+  )
+
+  return delegateChangedEvent
+}
+
+export function createDelegateVotesChangedEvent(
+  delegate: Address,
+  previousBalance: BigInt,
+  newBalance: BigInt
+): DelegateVotesChanged {
+  let delegateVotesChangedEvent = changetype<DelegateVotesChanged>(
+    newMockEvent()
+  )
+
+  delegateVotesChangedEvent.parameters = new Array()
+
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam("delegate", ethereum.Value.fromAddress(delegate))
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousBalance",
+      ethereum.Value.fromUnsignedBigInt(previousBalance)
+    )
+  )
+  delegateVotesChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "newBalance",
+      ethereum.Value.fromUnsignedBigInt(newBalance)
+    )
+  )
+
+  return delegateVotesChangedEvent
+}
+
+export function createMerkleRootChangedEvent(
+  merkleRoot: Bytes
+): MerkleRootChanged {
+  let merkleRootChangedEvent = changetype<MerkleRootChanged>(newMockEvent())
+
+  merkleRootChangedEvent.parameters = new Array()
+
+  merkleRootChangedEvent.parameters.push(
+    new ethereum.EventParam(
+      "merkleRoot",
+      ethereum.Value.fromFixedBytes(merkleRoot)
+    )
+  )
+
+  return merkleRootChangedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createTransferEvent(
+  from: Address,
+  to: Address,
+  value: BigInt
+): Transfer {
+  let transferEvent = changetype<Transfer>(newMockEvent())
+
+  transferEvent.parameters = new Array()
+
+  transferEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferEvent
+}
